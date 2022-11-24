9d5aae23fef4e4dbfdc80211b57d9d9d1b3e2bcb
HRyohni <matosevic.leo@gmail.com>
2022-11-24 03:26:42 +0100

    adding flask to main page Index

--- frontend_webservice/server.py
+++ frontend_webservice/server.py
@@ -1,18 +1,16 @@
-
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
-db =SQLAlchemy(app)
 
-class Todo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',test=" custom tag!")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#
