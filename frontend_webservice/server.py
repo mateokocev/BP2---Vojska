4129ef79f7351d116de00fd0ee1199e377c43626
HRyohni <matosevic.leo@gmail.com>
2022-11-29 01:54:21 +0100

    Login
    - mogućnost prijave pomoću md5 kripatiranja
    -spajanje backend na bazu podataka

--- frontend_webservice/server.py
+++ frontend_webservice/server.py
@@ -3,41 +3,58 @@ import mysql.connector
 
 
 # Database
-db= mysql.connector.connect(host='localhost',database='formula1',user='root',password='root')
-mycursor = db.cursor()
+#db= mysql.connector.connect(host='localhost',database='formula1',user='root',password='root')
+#mycursor = db.cursor()
 
-#mycursor.execute("Select isplacen_novac from sponzor_u_sezoni")
-#cijene=mycursor.fetchall()
-mycursor.execute("select ime_staze from staza;")
+#mycursor.execute("select ime_staze from staza;")
 
 
 #sve=mycursor.fetchmany(size=4) kolicina podataka
 
 #sve=mycursor.fetchone() jedan red 
 
-sve=mycursor.fetchall() #samo sve uzme... 
+#sve=mycursor.fetchall() #samo sve uzme... 
+
+
+
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/sauhdkuahsghdi7123z479123hj1jksdhusihadjkhsdfgliohjerofhj3489o5z923475z83475h") #for safety reasons tottaly not hackable
 def index():
     return render_template('index.html',test=123*123)
 
 @app.route("/kopnenaVojska/")
 def KopnenaVojska():
-    return render_template('kopnenaVojska.html',sve=sve)
+    return render_template('kopnenaVojska.html')
+
+
+
+
+
+
+                # login connecting to DataBase vojska
+vojska= mysql.connector.connect(host='localhost',database='vojska',user='root',password='root')
+krusor = vojska.cursor()
+
+
 
 
 
 # Route for handling the login page logic
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = ""
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        UpisIme=request.form['username']
+        UpisLozinka=request.form['password']
+
+
+        krusor.execute("select * from login where ime= '"+UpisIme+"' and md5('"+UpisLozinka+"') = lozinka;")
+       
+        if krusor.fetchone() == None:
+            error = 'Kriva lozinka pokusaj ponovno!'
         else:
-            return  "letss gooo" #redirect(url_for('index'))
+            return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
